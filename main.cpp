85bf4a13f3c9ee0ae4ada45995d864c1507d80ff
Mher <74150133+mhermelkonyan98@users.noreply.github.com>
2020-12-04 02:04:37 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <exception> // for std::invalid_argument exception
+#include <cmath> // for function pow()
+
+class Calculator {
+public:
+    int power(int n, int p) {
+        if (n < 0 || p < 0){
+            throw std::invalid_argument("n and p should be non-negative");
+        }
+        return pow(n, p);
+    }
+};
+
+int main() {
+    Calculator myCalculator = Calculator();
+    int T,n,p;
+    std::cin >> T;
+    while (T-- > 0){
+      if (scanf("%d %d", &n, &p) == 2){
+         try {
+               int ans = myCalculator.power(n,p);
+               std::cout << ans << std::endl;
+         }
+         catch (std::exception& e){
+             std::cerr << e.what() << std::endl;
+         }
+      }
+    }
+    return 0;
+}
